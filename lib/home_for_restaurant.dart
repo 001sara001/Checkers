6849ff150854b93a146146bb41e73bfff5ab28f1
001sara001 <malihatasnim530@gmail.com>
2024-01-30 15:34:41 +0600

    after restautant login

---
+++ lib/home_for_restaurant.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'rest_page.dart'; // Ensure the correct import for RestPage
+
+class MyHomePageForRestaurant extends StatefulWidget {
+
+  const MyHomePageForRestaurant({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePageForRestaurant> createState() => _MyHomePageForRestaurantState();
+}
+
+class _MyHomePageForRestaurantState extends State<MyHomePageForRestaurant> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+
+    );
+  }
+}
